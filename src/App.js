570b08268ba557942b115a18f2910b0202b7e5c2
Cathy V. Amamangpang <cathyamamangpang2@gmail.com>
2024-10-18 00:48:53 -0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import Header from './Header';
+import Videolist from './Videolist';
+import Footer from './Footer';
+import './App.css'; 
+
+function App() {
+  return (
+    <>
+    
+      <Header  />
+      <Description  name="Cathy" />
+      <Footer />
+      </>
+    
+  );
+}
+
+export default App;
---
+++ src/Videolist.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Videolist(props) {
+  return (
+    <>
+      <h2>Hello, {props.name}!</h2> 
+      <div className="card">Title Stats</div>
+      <div className="card">Title Stats</div>
+      <div className="card">Title Stats</div>
+    </>
+  );
+}
+
+export default Videolist;
