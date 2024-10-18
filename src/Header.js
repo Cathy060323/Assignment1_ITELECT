d3ee3dc3d5dc8664274af66aff816768c2c6f915
Cathy V. Amamangpang <cathyamamangpang2@gmail.com>
2024-10-18 00:56:46 -0700

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,18 +1,24 @@
 import React from 'react';
-import './App.css';
+import './Header.css'; // Assuming you might want separate CSS for Header if required
 
 const Header = () => {
   return (
     <header className="header">
-      <div className="logo"></div> {}
-      <nav>
+      <div className="logo">
+        <img src="/logo.png" alt="Logo" /> {/* Adjust logo path as needed */}
+      </div>
+      <div className="search-bar">
+        <input type="text" placeholder="Search" />
+      </div>
+      <nav className="navbar">
         <ul>
-          <li>Home</li>
-          <li>About</li>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">My Channel</a></li>
         </ul>
       </nav>
     </header>
   );
-};
+}
 
 export default Header;
