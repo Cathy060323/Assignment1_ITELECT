9dcc99030b7574ceb0c3467c41d30ca3796d05fe
Cathy V. Amamangpang <cathyamamangpang2@gmail.com>
2024-10-21 11:17:31 +0800

    Create Greetings.js

---
+++ src/Greetings.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Greetings = () => {
+  return (
+    <section className="greetings">
+      <h1>Welcome to the Home Page</h1>
+      
+      <a href="../">PAGE2</a>
+    </section>
+  );
+}
+
+export default Greetings;
