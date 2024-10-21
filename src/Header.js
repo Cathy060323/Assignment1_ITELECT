d97c8db7a70ee5a300118baaeb2c0cc965f9a107
Cathy V. Amamangpang <cathyamamangpang2@gmail.com>
2024-10-21 11:18:38 +0800

    Update Header.js

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import './Header.css'; // Assuming you might want separate CSS for Header if required
+import './Header.css'; 
 
 const Header = () => {
   return (
     <header className="header">
       <div className="logo">
-        <img src="/logo.png" alt="Logo" /> {/* Adjust logo path as needed */}
+        <img src="/logo.png" alt="Logo" /> 
       </div>
       <div className="search-bar">
         <input type="text" placeholder="Search" />
